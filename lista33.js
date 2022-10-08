c217bb03d6281561b2ec26c380976ed0e811d79e
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-08 14:15:46 -0300

    Término do exercício 2.

--- lista33.js
+++ lista33.js
@@ -1,8 +1,22 @@
 // Ex 1:
-console.log(RetornarVogais("eu fiz o meu almoço hoje"));
-function RetornarVogais(frase) {
-    var verificador = /[^aeiou]/gi;
-    var teste1 = frase.replace(verificador, '');
-    console.log("Frase: " + frase + " - Vogais: " + teste1);
-    return teste1.split('').length;
+// console.log(RetornarVogais("eu fiz o meu almoço hoje"))
+// function RetornarVogais(frase : string) : number {
+//     let verificador : RegExp = /[^aeiou]/gi
+//     let teste1 : string = frase.replace(verificador,'')
+//     console.log("Frase: " +frase+ " - Vogais: " +teste1)
+//     return teste1.split('').length
+// }
+// Ex 2:
+console.log(MostrarCaractereDoMeio("jogando"));
+function MostrarCaractereDoMeio(palavra) {
+    var arrayPalavra = palavra.split('');
+    console.log(arrayPalavra);
+    if (arrayPalavra.length % 2 == 0) {
+        var caracteresDoMeio = [];
+        caracteresDoMeio.push(arrayPalavra[(arrayPalavra.length / 2) - 1], arrayPalavra[arrayPalavra.length / 2]);
+        return caracteresDoMeio.join('');
+    }
+    else {
+        return arrayPalavra[(arrayPalavra.length / 2) - 0.5];
+    }
 }
--- lista33.ts
+++ lista33.ts
@@ -1,9 +1,27 @@
 // Ex 1:
-console.log(RetornarVogais("eu fiz o meu almoço hoje"))
 
-function RetornarVogais(frase : string) : number {
-    let verificador : RegExp = /[^aeiou]/gi
-    let teste1 : string = frase.replace(verificador,'')
-    console.log("Frase: " +frase+ " - Vogais: " +teste1)
-    return teste1.split('').length
+// console.log(RetornarVogais("eu fiz o meu almoço hoje"))
+
+// function RetornarVogais(frase : string) : number {
+//     let verificador : RegExp = /[^aeiou]/gi
+//     let teste1 : string = frase.replace(verificador,'')
+//     console.log("Frase: " +frase+ " - Vogais: " +teste1)
+//     return teste1.split('').length
+// }
+
+// Ex 2:
+
+console.log(MostrarCaractereDoMeio("jogando"))
+
+function MostrarCaractereDoMeio(palavra: string) : string{
+    let arrayPalavra : string[] = palavra.split('')
+    console.log(arrayPalavra)
+    if(arrayPalavra.length % 2 == 0){
+        let caracteresDoMeio :string[] = []
+        caracteresDoMeio.push(arrayPalavra[(arrayPalavra.length/2) - 1],arrayPalavra[arrayPalavra.length/2]) 
+        return caracteresDoMeio.join('') 
+    }else{
+        return arrayPalavra[(arrayPalavra.length/2) - 0.5]
+    }
+
 }
